15ee87965fb906c9515f5a2187b78a2e1c5ff936
robertramosastudillo <robertramosastudillo@gmail.com>
2019-11-04 14:34:19 -0500

    Pages

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
-// Javascript Vanilla
-
-// const element = document.createElement("h1");
-// element.innerText = "Hello React...";
-// const container = document.getElementById("root");
-// container.appendChild(element);
-
 import React from "react";
 import ReactDOM from "react-dom";
-import Card from "./components/Card";
-import Welcome from "./components/Welcome";
+import Exercises from "./pages/Exercises";
+
 import "bootstrap/dist/css/bootstrap.css";
 
 const container = document.getElementById("root");
 
 // ReactDOM.render(QUE ELEMENTO, DONDE);
-ReactDOM.render(
-  <div>
-    <Welcome username="Robert" />
-
-    <Card
-      title="Technique Guides"
-      description="Learn amazing street workout and calisthenics"
-      img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
-      leftColor="#a74cf2"
-      rightColor="#617bfb"
-    />
-  </div>,
-  container
-);
+ReactDOM.render(<Exercises />, container);
