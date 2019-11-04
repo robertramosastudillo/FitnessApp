52762231ddedfb1e32b653083a5236f3d39e6247
robertramosastudillo <robertramosastudillo@gmail.com>
2019-11-04 17:23:19 -0500

    Events

--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -1,8 +1,25 @@
 import React from "react";
 
 class ExerciseNew extends React.Component {
+  // Forma 1
+
+  // constructor(props) {
+  //   super(props);
+  //   this.handleClick = this.handleClick.bind(this);
+  // }
+
+  // handleClick() {
+  //   console.log(this);
+  // }
+
+  // Forma 2
+
+  handleClick = () => {
+    console.log("Clicked");
+  };
+
   render() {
-    return "ExerciseNew";
+    return <button onClick={this.handleClick}>Send</button>;
   }
 }
 
