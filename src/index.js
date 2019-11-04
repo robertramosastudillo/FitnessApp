67d0037ddddc584021e3769a97a1c3e32e61864b
robertramosastudillo <robertramosastudillo@gmail.com>
2019-11-04 14:27:21 -0500

    Functional Components

--- src/index.js
+++ src/index.js
@@ -8,18 +8,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Card from "./components/Card";
+import Welcome from "./components/Welcome";
 import "bootstrap/dist/css/bootstrap.css";
 
 const container = document.getElementById("root");
 
 // ReactDOM.render(QUE ELEMENTO, DONDE);
 ReactDOM.render(
-  <Card
-    title="Technique Guides"
-    description="Learn amazing street workout and calisthenics"
-    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
-    leftColor="#a74cf2"
-    rightColor="#617bfb"
-  />,
+  <div>
+    <Welcome username="Robert" />
+
+    <Card
+      title="Technique Guides"
+      description="Learn amazing street workout and calisthenics"
+      img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+      leftColor="#a74cf2"
+      rightColor="#617bfb"
+    />
+  </div>,
   container
 );
