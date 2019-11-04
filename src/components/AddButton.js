56ac1703d32264b49f36b76d6c5bb8593a4eedf6
robertramosastudillo <robertramosastudillo@gmail.com>
2019-11-04 16:48:56 -0500

    Dynamic Component Routing

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./styles/AddButton.css";
 import buttomImg from "../images/add.png";
+import { Link } from "react-router-dom";
 
 function AddButton(props) {
   return (
-    <a href="/exercise/new">
+    <Link to="/exercise/new">
       <img src={buttomImg} alt="" className="Fitness-Add" />
-    </a>
+    </Link>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Exercises from "./pages/Exercises";
+import App from "./components/App";
 
 import "bootstrap/dist/css/bootstrap.css";
 
 const container = document.getElementById("root");
 
-ReactDOM.render(<Exercises />, container);
+ReactDOM.render(<App />, container);
