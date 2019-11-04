a487e5287b59bdd091d6e004f1162aa194ea6f1d
robertramosastudillo <robertramosastudillo@gmail.com>
2019-11-04 12:53:34 -0500

    My first component

--- src/index.js
+++ src/index.js
@@ -9,33 +9,9 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-
-const user = {
-  firstName: "Robert",
-  lastName: "Ramos",
-  avatar:
-    "https://i.pinimg.com/originals/39/5a/91/395a91db78d3fe72c3a8f944877010db.jpg"
-};
-
-function getName(user) {
-  return `${user.firstName} ${user.lastName}`;
-}
-
-function getGreeting(user) {
-  if (user) {
-    return <h1>Hello {getName(user)}</h1>;
-  }
-  return <h1>Hello Stranger</h1>;
-}
-
-const element = (
-  <div>
-    <h1>{getGreeting(user)}</h1>
-    <img src={user.avatar} />
-  </div>
-);
+import Card from "./components/Card";
 
 const container = document.getElementById("root");
 
 // ReactDOM.render(QUE ELEMENTO, DONDE);
-ReactDOM.render(element, container);
+ReactDOM.render(<Card />, container);
