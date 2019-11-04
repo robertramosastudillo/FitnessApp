3e90cc7d9d16ed2843d1bf8929b9abd27f8170df
robertramosastudillo <robertramosastudillo@gmail.com>
2019-11-04 17:14:41 -0500

    ES7 & Babel

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -3,12 +3,18 @@ import "./styles/AddButton.css";
 import buttomImg from "../images/add.png";
 import { Link } from "react-router-dom";
 
-function AddButton(props) {
-  return (
-    <Link to="/exercise/new">
-      <img src={buttomImg} alt="" className="Fitness-Add" />
-    </Link>
-  );
-}
+const AddButton = () => (
+  <Link to="/exercise/new">
+    <img src={buttomImg} alt="" className="Fitness-Add" />
+  </Link>
+);
+
+// function AddButton() {
+//   return (
+//     <Link to="/exercise/new">
+//       <img src={buttomImg} alt="" className="Fitness-Add" />
+//     </Link>
+//   );
+// }
 
 export default AddButton;
--- src/components/ExerciseList.js
+++ src/components/ExerciseList.js
@@ -1,23 +1,40 @@
 import React from "react";
 import Card from "./Card";
 
-function ExerciseList(props) {
-  return (
-    <div>
-      {props.exercises.map((exercise, i) => {
-        return (
-          <Card
-            key={i}
-            title={exercise.title}
-            description={exercise.description}
-            img={exercise.img}
-            leftColor={exercise.leftColor}
-            rightColor={exercise.rightColor}
-          />
-        );
-      })}
-    </div>
-  );
-}
+const ExerciseList = ({ exercises }) => (
+  <div>
+    {exercises.map(exercise => {
+      return (
+        <Card
+          key={exercise.id}
+          title={exercise.title}
+          description={exercise.description}
+          img={exercise.img}
+          leftColor={exercise.leftColor}
+          rightColor={exercise.rightColor}
+        />
+      );
+    })}
+  </div>
+);
+
+// function ExerciseList(props) {
+//   return (
+//     <div>
+//       {props.exercises.map((exercise, i) => {
+//         return (
+//           <Card
+//             key={i}
+//             title={exercise.title}
+//             description={exercise.description}
+//             img={exercise.img}
+//             leftColor={exercise.leftColor}
+//             rightColor={exercise.rightColor}
+//           />
+//         );
+//       })}
+//     </div>
+//   );
+// }
 
 export default ExerciseList;
