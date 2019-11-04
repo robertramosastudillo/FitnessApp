0656c0fbbd6721af950a480c431393f58ffc0c43
robertramosastudillo <robertramosastudillo@gmail.com>
2019-11-04 12:28:18 -0500

    JSX

--- src/index.js
+++ src/index.js
@@ -10,7 +10,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = <h1>Hello React</h1>;
+const user = {
+  firstName: "Robert",
+  lastName: "Ramos",
+  avatar:
+    "https://i.pinimg.com/originals/39/5a/91/395a91db78d3fe72c3a8f944877010db.jpg"
+};
+
+function getName(user) {
+  return `${user.firstName} ${user.lastName}`;
+}
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello {getName(user)}</h1>;
+  }
+  return <h1>Hello Stranger</h1>;
+}
+
+const element = (
+  <div>
+    <h1>{getGreeting(user)}</h1>
+    <img src={user.avatar} />
+  </div>
+);
+
 const container = document.getElementById("root");
 
 // ReactDOM.render(QUE ELEMENTO, DONDE);
