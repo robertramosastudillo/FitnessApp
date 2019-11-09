ce91cf995446c2f77b111c9911b850d696ca2d48
robertramosastudillo <robertramosastudillo@gmail.com>
2019-11-09 13:38:44 -0500

    Effect Hook

--- src/pages/ExercisesContainer.js
+++ src/pages/ExercisesContainer.js
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Loading from "../components/Loading";
 import FatalError from "./500";
 import Exercises from './Exercises';
 
 const ExercisesContainer = () => {
   const [ data, setData ] = useState([]);
-  const [ loading, setLoading ] = useState(false);
+  const [ loading, setLoading ] = useState(true);
   const [ error, setError ] = useState(null);
+
+  useEffect(() => {
+    const fetchExercises = async () => {
+      try {
+        let res = await fetch("http://localhost:8000/api/exercises");
+        let data = await res.json();
+        setData(data)
+        setLoading(false)
+      } catch (error) {
+        setLoading(false)
+        setError(error)
+      }
+    };
+    fetchExercises();
+  }, [])
 
     if (loading) return <Loading />;
     if (error) return <FatalError />;
@@ -16,39 +31,3 @@ const ExercisesContainer = () => {
 }
 
 export default ExercisesContainer;
-
-// class ExercisesContainer extends React.Component {
-//   state = {
-//     data: [],
-//     loading: true,
-//     error: null
-//   };
-
-//   async componentDidMount() {
-//     await this.fetchExercises();
-//   }
-
-//   fetchExercises = async () => {
-//     try {
-//       let res = await fetch("http://localhost:8000/api/exercises");
-//       let data = await res.json();
-//       this.setState({
-//         data,
-//         loading: false
-//       });
-//     } catch (error) {
-//       this.setState({
-//         loading: false,
-//         error
-//       });
-//     }
-//   };
-
-//   render() {
-//     if (this.state.loading) return <Loading />;
-//     if (this.state.error) return <FatalError />;
-
-//     return <Exercises data={this.state.data}/>
-//   }
-// }
-// export default ExercisesContainer;
