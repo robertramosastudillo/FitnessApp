4843261bbbf4104f0241950a7c32e9b5d930073a
robertramosastudillo <robertramosastudillo@gmail.com>
2019-11-09 11:41:47 -0500

    Presentational & Container Components

---
+++ src/pages/ExerciseNewContainer.js
@@ -0,0 +1,70 @@
+import React from "react";
+import '../components/styles/ExerciseNew.css';
+import FatalError from "./500";
+import ExerciseNew from './exerciseNew';
+
+class ExerciseNewContainer extends React.Component {
+  state = {
+    form: {
+      title: "",
+      description: "",
+      img: "",
+      leftColor: "",
+      rightColor: ""
+    },
+    loading: false,
+    error: null
+  };
+
+  handleChange = e => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  handleSubmit = async e => {
+    this.setState({
+      loading: true
+    });
+
+    e.preventDefault();
+    try {
+      let config = {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.state.form)
+      };
+
+      let res = await fetch("http://localhost:8000/api/exercises", config);
+      let json = await res.json();
+
+      console.log(json);
+
+      this.setState({
+        loading: false
+      });
+
+      this.props.history.push('/exercise');
+
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error
+      });
+    }
+  };
+
+  render() {
+    if (this.state.error) return <FatalError />;
+
+    return <ExerciseNew form={this.state.form} onChange={this.handleChange} onSubmit={this.handleSubmit}/>
+  }
+}
+
+export default ExerciseNewContainer;
